417cb8448c1065f17ca3f183f8283942291d45ce
123Fatima <faty_hdz_00@hotmail.com>
2016-09-14 22:47:51 -0600

    CalculadoraSwing
    Calculadora con Swing

---
+++ CaluladoraS/src/edu/ipn/cecyt9/calculadora/Logaritmo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.ipn.cecyt9.calculadora;
+
+/**
+ *
+ * @author intel
+ */
+public class Logaritmo implements calcular {
+    public double operacion(double num1){
+        return Math.log(num1);
+    }
+
+    @Override
+    public double operacion() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }    
+}
